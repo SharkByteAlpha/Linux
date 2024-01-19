f9680961c570e0812594170bf2b3126e2fbc32f6
chrismaslion <52667155+chrismaslion@users.noreply.github.com>
2024-01-19 07:44:30 -0500

    Add files via upload

---
+++ helpme.sh
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2758
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
+{\colortbl;\red255\green255\blue255;\red8\green64\blue128;\red244\green244\blue244;\red11\green12\blue12;
+}
+{\*\expandedcolortbl;;\cssrgb\c392\c32941\c57647;\cssrgb\c96471\c96471\c96471;\cssrgb\c4706\c5098\c5490;
+}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
+#!/bin/sh\cf4 \
+\
+Sudo ufw enable\
+\
+\
+}
