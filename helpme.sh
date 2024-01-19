e842c36dc4e7940a1aa3a4537e53374747afc4d8
chrismaslion <52667155+chrismaslion@users.noreply.github.com>
2024-01-19 08:03:59 -0500

    Update helpmeet.sh

--- helpme.sh
+++ helpme.sh
@@ -1,17 +1,34 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2758
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
-{\colortbl;\red255\green255\blue255;\red8\green64\blue128;\red244\green244\blue244;\red11\green12\blue12;
-}
-{\*\expandedcolortbl;;\cssrgb\c392\c32941\c57647;\cssrgb\c96471\c96471\c96471;\cssrgb\c4706\c5098\c5490;
-}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+#!/bin/bash
 
-\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
-#!/bin/sh\cf4 \
-\
-Sudo ufw enable\
-\
-\
-}
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
+
+# Update and install UFW if not installed
+apt update
+apt install -y ufw
+
+# Set default policies
+ufw default deny incoming
+ufw default allow outgoing
+
+# Allow SSH connections
+ufw allow ssh
+
+# Prompt user to enter ports
+read -p "Enter the ports you want to open (space-separated): " ports
+
+# Allow specified ports
+for port in $ports; do
+    ufw allow "$port"
+done
+
+# Enable UFW
+ufw enable
+
+# Enable logging of dropped packets
+ufw logging on
+
+echo "UFW is now configured. Only SSH connections are allowed, and specified ports are open."
